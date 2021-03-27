c88a29f36517351b61ce4750ae0b5918578a7ac1
Denis <dineresc@gmail.com>
2021-03-27 09:30:37 -0300

    importando material ui

---
+++ src/components/CadastroFormulario/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CadastroFormulario() {
+    return(
+        <form>
+            <label>Nome:</label>
+            <input type="text"/>
+        </form>
+    )
+}
+
+export default CadastroFormulario;
